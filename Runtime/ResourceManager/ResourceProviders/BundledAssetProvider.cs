02a9406b6b9843810239f050eb7ce907b6cd0c3d
James Kim <eodnjs2998@gmail.com>
2024-10-30 18:58:01 +0900

    added detailed log

--- Runtime/ResourceManager/ResourceProviders/BundledAssetProvider.cs
+++ Runtime/ResourceManager/ResourceProviders/BundledAssetProvider.cs
@@ -1,3 +1,4 @@
+using UnityEngine.AddressableAssets.Util;
 using UnityEngine.Assertions;
 
 namespace UnityEngine.AddressableAssets.ResourceProviders
@@ -7,8 +8,14 @@ namespace UnityEngine.AddressableAssets.ResourceProviders
         public AsyncOperation LoadAsync<T>(AssetBundle bundle, Address address)
         {
             var name = address.Name();
+            L.I($"[Addressables] AssetBundle.LoadAssetAsync: {name}");
             Assert.IsTrue(bundle.Contains(name), $"Bundle does not contain asset: {name}");
-            return bundle.LoadAssetAsync(name, typeof(T));
+
+            var op = bundle.LoadAssetAsync(name, typeof(T));
+#if DEBUG
+            op.completed += op2 => L.I($"[Addressables] AssetBundle.LoadAssetAsync completed: key={name}, asset={((AssetBundleRequest) op2).asset}");
+#endif
+            return op;
         }
 
         public object GetResult(AsyncOperation op)
--- Runtime/ResourceManager/ResourceProviders/SceneProvider.cs
+++ Runtime/ResourceManager/ResourceProviders/SceneProvider.cs
@@ -8,12 +8,18 @@ namespace UnityEngine.AddressableAssets.ResourceProviders
     {
         public AsyncOperation LoadAsync<T>(AssetBundle bundle, Address address)
         {
+            var name = address.Name();
+            L.I($"[Addressables] SceneManager.LoadSceneAsync: {name}");
             Assert.AreEqual(typeof(Scene), typeof(T));
-            Assert.IsTrue(bundle.Contains(address.Name()), $"Bundle does not contain scene: {address.Name()}");
-            var op = SceneManager.LoadSceneAsync(address.Name(), LoadSceneMode.Additive);
+            Assert.IsTrue(bundle.Contains(name), $"Bundle does not contain scene: {name}");
+
+            var op = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
             op.allowSceneActivation = true;
             var scene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
             AsyncOpPayloads.SetScene(op, scene);
+#if DEBUG
+            op.completed += _ => L.I($"[Addressables] SceneManager.LoadSceneAsync completed: {name}");
+#endif
             return op;
         }
 
