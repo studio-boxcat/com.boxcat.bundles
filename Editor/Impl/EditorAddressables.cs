c1ee5e08c32bfe4cebae6841d2f9499e85018834
James Kim <eodnjs2998@gmail.com>
2025-04-22 15:42:21 +0900

    wip

--- Editor/Impl/EditorAddressables.cs
+++ Editor/Impl/EditorAddressables.cs
@@ -5,6 +5,11 @@ namespace UnityEditor.AddressableAssets
 {
     public static class EditorAddressables
     {
+        public static bool Contains(string address)
+        {
+            return AddressableCatalog.Default.ContainsEntry(address);
+        }
+
         public static T LoadAsset<T>(Address address) where T : Object
         {
             return AddressableCatalog.Default.GetEntry(address).LoadAssetWithType<T>();
