43bc0c23fae2a42a5bf771f853e1c2206d7786ab
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-01 22:51:12 +0900

    added comment for Address

--- Editor/Utils/AddressCodeGen.cs
+++ Editor/Utils/AddressCodeGen.cs
@@ -1,21 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Bundles.Editor
 {
     internal static class AddressCodeGen
     {
         public static void GenerateCode(AssetCatalog catalog, string path)
         {
-            var b = new CodeBuilder();
-            b.Using("Bundles");
-            b.Blank();
-
-            using (b.Public_Static_Class("Address"))
+            var list = new List<(string Name, Address Value, string Comment)>();
+            foreach (var g in catalog.Groups.Where(g => g.BundleId.AddressAccess()))
             {
-                var addressList = catalog.GetAddressList();
-                foreach (var addressName in addressList)
+                var groupName = g.Key.Value;
+                var bundleId = g.BundleId.Name();
+                foreach (var e in g.Entries.Where(e => e.Address.NotEmpty()))
                 {
+                    var addressName = e.Address;
                     var address = AddressUtils.Hash(addressName);
-                    b.Public_Const_Field("Address", addressName, $"(Address) {(uint) address}u", comment: address.Hex());
+                    // b.EnumValue(addressName, $"{(uint) address}u", comment: comment);
+                    list.Add((addressName, address, $"0x{address.Hex()}, guid={e.GUID.Value}, group={groupName}, bundleId={bundleId}"));
                 }
+            }
+            list.Sort((a, b) => a.Name.CompareToOrdinal(b.Name));
+
+            var b = new CodeBuilder();
+
+            using (b.Enum("Address", "uint"))
+            {
+                foreach (var (name, value, comment) in list)
+                    b.EnumValue(name, $"{(uint) value}u", comment: comment);
             }
 
             b.WriteTo(path);
