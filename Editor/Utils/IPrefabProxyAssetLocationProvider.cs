2cdfccbe613984d6700c6c8fba699abf03916b4e
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-16 15:40:37 +0900

    prefab proxy

---
+++ Editor/Utils/IPrefabProxyAssetLocationProvider.cs
@@ -0,0 +1,19 @@
+#nullable enable
+
+using UnityEngine;
+
+namespace Bundles.Editor
+{
+    public interface IPrefabProxyAssetLocationProvider : IPrefabProxyAssetProvider
+    {
+        new AssetLocation? Get(PrefabProxy proxy);
+
+        GameObject? IPrefabProxyAssetProvider.Get(PrefabProxy proxy)
+        {
+            var loc = Get(proxy);
+            return loc.TryGetValue(out var locVal)
+                ? EditorBundles.LoadAsset<GameObject>(locVal)
+                : null;
+        }
+    }
+}
