e7bb6e9d805835aeb43a3ca87f95bb3a4d132ac6
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-21 10:43:31 +0900

    refactor AddOnComplete methods to support nullable object parameters

--- Runtime/AsyncOperations/CompletedOp.cs
+++ Runtime/AsyncOperations/CompletedOp.cs
@@ -21,11 +21,11 @@ namespace Bundles
         public TResult WaitForCompletion() => _result;
 
         public void AddOnComplete(Action<TResult> onComplete) => onComplete(_result);
-        public void AddOnComplete(Action<TResult, object> onComplete, object payload) => onComplete(_result, payload);
+        public void AddOnComplete(Action<TResult, object?> onComplete, object? payload) => onComplete(_result, payload);
         public void AddOnComplete(Action<TResult, int> onComplete, int payload) => onComplete(_result, payload);
-        public void AddOnComplete(Action<TResult, object, int> onComplete, object payloadObj, int payloadInt) => onComplete(_result, payloadObj, payloadInt);
-        public void AddOnComplete(Action<IAssetOp<TResult>, TResult, object> onComplete, object payload) => onComplete(this, _result, payload);
+        public void AddOnComplete(Action<TResult, object?, int> onComplete, object? payloadObj, int payloadInt) => onComplete(_result, payloadObj, payloadInt);
+        public void AddOnComplete(Action<IAssetOp<TResult>, TResult, object?> onComplete, object? payload) => onComplete(this, _result, payload);
         public void AddOnComplete(Action<IAssetOp<TResult>, TResult, int> onComplete, int payload) => onComplete(this, _result, payload);
-        public void AddOnComplete(Action<IAssetOp<TResult>, TResult, object, int> onComplete, object payloadObj, int payloadInt) => onComplete(this, _result, payloadObj, payloadInt);
+        public void AddOnComplete(Action<IAssetOp<TResult>, TResult, object?, int> onComplete, object? payloadObj, int payloadInt) => onComplete(this, _result, payloadObj, payloadInt);
     }
 }
